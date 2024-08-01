d2886f1ae7cde564bb82625a2f0b7d252b3dc960
Sandrin Hunkeler <sandrin@hunkeler.ws>
2024-08-01 20:18:27 +0200

    added avg, stddev

--- src/app/scale/scale.component.ts
+++ src/app/scale/scale.component.ts
@@ -5,6 +5,8 @@ import {Chart, ChartConfiguration, ChartData, ChartOptions, registerables} from 
 Chart.register(...registerables);
 
 import {BaseChartDirective} from "ng2-charts";
+import {min} from "rxjs";
+import {each, isNumber} from "chart.js/helpers";
 
 @Component({
   selector: 'app-scale',
@@ -18,6 +20,8 @@ export class ScaleComponent {
   messages: string[] = [];
   title: string = "PiggyScale";
   counter: number = 0;
+  stdDev: string = "0";
+
 
   @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
 
@@ -28,25 +32,82 @@ export class ScaleComponent {
   }
 
   addDataToChart(time: Date, value: number) {
-    if (this.lineChartData.labels && this.lineChartData.datasets) {
+    const rawData : (number | null)[] = this.lineChartData.datasets[0].data as (number | null)[];
+    const averageData : (number | null)[] = this.lineChartData.datasets[1].data as (number | null)[];
+    const label: (number | null)[] = this.lineChartData.labels as (number | null)[];
+
+    if (label && rawData && averageData) {
       this.cd.detectChanges();
-      this.lineChartData.labels.push(this.counter);
-      this.lineChartData.datasets[0].data.push(value);
-      while (this.lineChartData.datasets[0].data.length < 20) {
-        this.lineChartData.labels.push(null);
-        this.lineChartData.datasets[0].data.push(null);
-        console.log("pushed");
+      label.push(this.counter);
+      rawData.push(value);
+
+      let len : number = 10;
+      let total : number = 0;
+      let notNull: number = 0;
+      while (len > 0) {
+        const nextV: number | null = rawData[rawData.length - len];
+        if (nextV !== null && !isNaN(nextV)) {
+          total += nextV;
+          notNull++;
+        }
+        len--;
+      }
+      const avg : number = total /  notNull;
+      if (total && notNull){
+        averageData.push(avg);
       }
 
-      if (this.lineChartData.datasets[0].data.length > 20) {
-        this.lineChartData.labels.shift();
-        this.lineChartData.datasets[0].data.shift();
+      len = 10;
+      total = 0;
+
+      if (avg && notNull){
+        while (len > 0) {
+          const nextV: number | null = rawData[rawData.length - len];
+          if (nextV !== null && !isNaN(nextV)) {
+            total += Math.pow(nextV - avg, 2);
+          }
+          len--;
+        }
+        if (total){
+          this.stdDev= Math.pow(total /  notNull, 0.5).toFixed(2);
+        }
+      }
+
+      let width: number = 50;
+      [rawData, averageData, label].forEach(function(lst: (number | null)[])
+      {
+        while (lst.length < width) {
+          lst.push(null)
+        }
+      })
+
+      if (rawData.length > width) {
+        label.shift();
+        rawData.shift();
+        averageData.shift();
       }
     }
+
     this.counter++;
     if (this.chart) {
       this.chart.update();
     }
+  }
+
+  public lastestAverage() : string {
+    let latestV : number | null = this.lineChartData.datasets[1].data[this.lineChartData.datasets[1].data.length-1] as number | null;
+    if (isNumber(latestV)){
+      return latestV.toFixed(2);
+    }
+    return "0";
+  }
+
+  public lastestRaw() : string {
+    let latestV : number | null = this.lineChartData.datasets[0].data[this.lineChartData.datasets[0].data.length-1] as number | null;
+    if (isNumber(latestV)){
+      return latestV.toFixed(2);
+    }
+    return "0";
   }
 
   public lineChartData: ChartConfiguration['data'] = {
@@ -55,6 +116,12 @@ export class ScaleComponent {
         data: [],
         label: 'Measurements',
         borderColor: '#3e95cd',
+        fill: false,
+      },
+      {
+        data: [],
+        label: 'Average',
+        borderColor: '#333333',
         fill: false,
       }
     ],
@@ -69,7 +136,7 @@ export class ScaleComponent {
     },
     scales: {
       x: {
-        display: true,
+        display: false,
         title: {
           display: true,
           text: 'Measurements'
